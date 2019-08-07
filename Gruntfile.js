08a85accb591f5a7880b6e4f79dfa1cb8ef6b909
Mark Chen <mchen@uts-canada.com>
2019-08-07 09:58:39 -0600

    port change to 9000 (default)

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 8000,
+        port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
         livereload: 35729
